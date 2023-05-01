456c078e27574c1e5dd6e386973c9de731f44b40
Derek Chan <derekchancts@yahoo.com.hk>
2023-05-01 16:15:24 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const path = require('path')
+
+app.use(cors())
+// app.use(cors({ origin: 'https://localhost' }))
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+
+app.get('/names', (req, res) => {
+  res.send('Derek')
+})
+
+app.listen(5000, () => console.log('Server is running on port 5000'))
---
+++ frontend/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+// import './App.css'
+
+
+function App() {
+  const [userName, setUserName] = useState(''); 
+  // console.log({userName})
+
+  // const [ip, setIP] = useState("");
+  // console.log({ip})
+
+
+  // const getData = async () => {
+  //   const res = await axios.get("https://api.ipify.org/?format=json");
+  //   console.log(res.data);
+  //   setIP(res.data.ip);
+  // };
+
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+  
+
+
+  useEffect(() => {
+    getNames();
+  }, [])
+
+
+  const getNames = async () => {
+    // const response = await axios('http://localhost:5000/names')
+    // const response = await axios(`http://${ip}:5000/names`)
+    const response = await axios('http://192.168.1.103:5000/names')
+    // const response = await axios('/names')
+    // console.log({response})
+    setUserName(response.data)
+  }
+
+
+  return (
+    <>
+      <h1>My Website</h1>
+      <h3>My name is {userName}</h3>
+    </>
+  )
+}
+
+export default App
