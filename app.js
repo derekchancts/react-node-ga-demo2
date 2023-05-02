d92c76813e1392b72acf3c95578f87ff5c929094
Derek Chan <derekchancts@yahoo.com.hk>
2023-05-02 15:59:01 +0800

    added /api routes

--- app.js
+++ app.js
@@ -13,4 +13,17 @@ app.get('/names', (req, res) => {
   res.send('Derek Chan')
 })
 
+app.get('/api/', (req, res) => {
+  res.json({ msg: 'Home', status: 200 })
+})
+
+app.get('/api//hello', (req, res) => {
+  res.json({ msg: 'Hello', status: 200 })
+})
+
+app.get('/api/dashboard', (req, res) => {
+  res.json({ msg: 'dashboard', status: 200 })
+})
+
+
 app.listen(5000, () => console.log('Server is running on port 5000'))
